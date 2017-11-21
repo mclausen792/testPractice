4f09830a66c1560cc1dbd3f42420a24cafe3314c
Makayla Clausen <mclausen792@gmail.com>
2017-11-21 15:26:51 -0700

    Learning Tests

---
+++ yellPhrase.js
@@ -0,0 +1,12 @@
+let yellPhrase = (phrase) => {
+  if (!phrase || typeof phrase !== 'string') {
+    return 'Please input a phrase'
+  }
+  return phrase.toUpperCase()
+}
+
+
+
+module.exports = {
+  yellPhrase
+}
